be39f589678f5517a339c1a5a24a861369f23d91
April <april@groover.co>
2022-08-27 20:28:30 +0200

    feat: nice comfy message for impatient users :>

--- src/views/PersonByImdbId.vue
+++ src/views/PersonByImdbId.vue
@@ -5,5 +5,17 @@ export default defineComponent({});
 </script>
 
 <template>
-  <div>person</div>
+  <div>
+    <p>Comming soon gamers, I promise</p>
+    <p>
+      For now here's the
+      <a
+        class="inline-block bg-blue-500 p-1 hover:bg-yellow-500"
+        target="_blank"
+        :href="`https://www.imdb.com/name/${$route.params.imdbId}`"
+        >imdb link</a
+      >
+      to this person
+    </p>
+  </div>
 </template>
