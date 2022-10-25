d5c740dd4c689f847562d197f49e4e8f903e5632
April <april@groover.co>
2022-10-25 19:53:22 +0200

    chore: small config change + made job launch imediate

--- worker-layer/index.ts
+++ worker-layer/index.ts
@@ -3,11 +3,14 @@ import "#/workers/processSources";
 import "#/workers/scanDirectoryForEntities";
 import "#/workers/convertFileToWebMp4";
 import "#/workers/bindFileToDbEntity";
-import { QueueProcessSources } from "./queues/processSources";
+import { QueueProcessSources } from "#/queues/processSources";
 
 import { addTvDbTokenToProcessEnv } from "~/addTvDbTokenToProcessEnv";
 
+console.log("haha");
+
 addTvDbTokenToProcessEnv().then(() => {
+  console.log("hehe");
   const queue = QueueProcessSources();
 
   // Initial job calls
@@ -15,7 +18,7 @@ addTvDbTokenToProcessEnv().then(() => {
     // Repeat scan sources every 5 hours
     repeat: {
       every: 1000 * 60 * 60 * 5,
-      immediately: true,
     },
   });
+  queue.add("processSources");
 });
